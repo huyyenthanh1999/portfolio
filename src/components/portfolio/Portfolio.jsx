e5ebdf9afb7e938a54a1a10f1045be6e4f846e62
Huy Pham <huypham.glinking@gmail.com>
2022-01-02 20:50:58 +0700

    init

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList"
+import { 
+    featuredPortfolio,
+    webPortfolio,
+    mobilePortfolio,
+    designPortfolio,
+    contentPortfolio    
+    } from "../../data"
+import "./portfolio.scss"
+
+export default function Portfolio() {
+    const [selected, setSelected] = useState("featured");
+    const [data, setData] = useState([]);
+
+    const list = [
+        {
+            id: "featured",
+            title: "Featured"
+        },
+        {
+            id: "web",
+            title: "Web"
+        },
+        {
+            id: "mobile",
+            title: "Mobile App"
+        },
+        {
+            id: "design",
+            title: "Design"
+        },
+        {
+            id: "content",
+            title: "Content"
+        }
+    ]
+
+    useEffect(() => {
+        switch(selected){
+            case "featured":
+                setData(featuredPortfolio);
+                break;
+            case "web":
+                setData(webPortfolio);
+                break;
+            case "mobile":
+                setData(mobilePortfolio);
+                break;
+            case "design":
+                setData(designPortfolio);
+                break;
+            case "content":
+                setData(contentPortfolio);
+                break;
+            default:
+        }
+    },[selected])
+
+    return (
+        <div className="portfolio" id="portfolio">
+            <h1>Portfolio</h1>
+            <ul>
+                {list.map((li, index) => (
+                    <PortfolioList 
+                        key={index}
+                        active={selected  === li.id} 
+                        title={li.title}
+                        setSelected={setSelected}
+                        id={li.id}
+                    />
+                ))}
+            </ul>
+            <div className="container">
+                {data.map((item) => (
+                    <div className="item" key={item.id}>
+                        <img src={item.img} alt="" />
+                        <h3>{item.title}</h3>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
---
+++ src/components/portfolioList/PortfolioList.jsx
@@ -0,0 +1,12 @@
+import "./portfolioList.scss"
+
+export default function PortfolioList({title, active, setSelected, id}) {
+    return (
+        <li 
+            className={"portfolioList" + (active ? " active" : "")}
+            onClick={() => setSelected(id)}
+        >
+            {title}
+        </li>
+    )
+}
